f06930fec602633683f8efb6d22e05c2a47cb017
Evgeny Shpilevsky <evgeny@shpilevsky.com>
2015-05-15 09:20:47 +0000

    Update dependencies. Add new 62036 dependencies

--- make.js
+++ make.js
@@ -1,13 +1,13 @@
-var http = require('http');
+var https = require('https');
 var fs = require('fs');
 
 var options = {
-  hostname: 'isaacs.iriscouch.com',
-  port: 80,
+  hostname: 'skimdb.npmjs.com',
+  port: 443,
   path: '/registry/_all_docs'
 };
 
-var req = http.request(options, function(res) {
+var req = https.request(options, function(res) {
   var body = '';
 
   res.setEncoding('utf8');
@@ -16,25 +16,25 @@ var req = http.request(options, function(res) {
   });
 
   res.on('end', function() {
+    var packageJson = require("./package.json");
+    var currentDependencies = packageJson.dependencies;
     var json = JSON.parse(body);
-    var packageJson = {
-      name: "whole-npm",
-      version: "1.0.0",
-      scripts: {
-        make: 'make.js'
-      },
-      description: "Just whole npm",
-      author: "Evgeny Shpilevsky",
-      license: "MIT",
-      dependencies: {}
-    };
 
+    packageJson.dependencies = {};
+
+    var foundNewDeps = 0;
     json.rows.forEach(function(package){
       // Ok, really not whole npm
       if (package.id !== 'whole-npm') {
+        if (!currentDependencies[package.id]) {
+          foundNewDeps++;
+        }
+
         packageJson.dependencies[package.id] = '*';
       }
     });
+
+    console.log('Found', foundNewDeps, 'new deps');
 
     fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
   });
